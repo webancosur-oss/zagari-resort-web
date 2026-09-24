e347e9ff0ba2c1cce1ed50a9cea03f18b81951bc
webancosur-oss <web.ancosur@ancosur.com>
2026-09-24 11:22:44 -0500

    feat: add environment variable for Mapbox token in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,13 @@ import type {
 
 
 
+
 const nextConfig: NextConfig = {
+
+   env: {
+    NEXT_PUBLIC_MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+  },
+  
   allowedDevOrigins: [
     "192.168.1.131"
   ],
